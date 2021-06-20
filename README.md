dc5f55bdfc0fd2bb6bcd3f2ec4e3988e5aef227b
Trung Quân <quanlightning17@gmail.com>
2021-06-20 14:17:02 +0700

    Push all code.

---
+++ README.md
@@ -0,0 +1,16 @@
+# trungquandev-cv-online
+My CV Online! - https://cv.trungquandev.com
+
+# Demo
+![trungquandev-cv-online](https://raw.githubusercontent.com/trungquan17/nodejs-two-factor-authentication-2fa/master/imagesDemo/nodejs-two-factor-authentication-practice-workflow-trungquandev-02.jpg)
+
+## Signature
+Author: Trung Quân
+
+Blog: https://trungquandev.com/
+
+CV: https://cv.trungquandev.com/
+
+"A little bit of fragrance always clings to the hands that gives flowers!"
+
+Thanks for watching!
---
+++ public/js/resume.js
@@ -0,0 +1,52 @@
+// <!-- Author: https://trungquandev.com -->
+function zoomImages(className) {
+  $(`.${className}`).unbind("click").on("click", function() {
+    $("#div-zoom-area").css("display", "block");
+    $("#img-zoom-area-content").attr("src", $(this)[0].src);
+
+    $("#div-zoom-area").on("click", function() {
+      $(this).css("display", "none");
+    });
+  });
+}
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $("a.js-scroll-trigger[href*='#']:not([href='#'])").click(function() {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: (target.offset().top)
+        }, 500, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#sideNav"
+  });
+  
+  // Say hello when access page.
+  if (window.innerWidth < 992) {
+    // Default position of notify is bottom-right
+    alertify.success("Have a good day, everyone! I'm Trung Quan from Viet Nam.", 7);
+  } else {
+    alertify.set("notifier","position", "top-right");
+    alertify.success("Have a good day, everyone! I'm Trung Quan from Viet Nam.", 7);
+  }
+
+  // Zoom images that have class name: img-zoomable
+  zoomImages("img-zoomable");
+
+})(jQuery); // End of use strict
